3d0acb70b8aeb7e599632e0377ec02bf37ee2abc
wanyj <2026804718@qq.com>
2026-09-26 18:31:50 +0800

    docs: v5.2

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 # 普通镜像构建，随系统版本构建 amd/arm
-docker build -t wanyj/codefreex:5.1 -f ./Dockerfile .
+docker build -t wanyj/codefreex:5.2 -f ./Dockerfile .
--- push.sh
+++ push.sh
@@ -8,7 +8,7 @@ set -e
 ALIYUN_REGISTRY="registry.cn-wulanchabu.aliyuncs.com"
 NAMESPACE="wanyj"
 IMAGE_NAME="codefreex"
-IMAGE_TAG="5.1"
+IMAGE_TAG="5.2"
 
 # 读取本地配置文件
 if [ -f ".local-config" ]; then
